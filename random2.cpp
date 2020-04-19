1ec666c6cad511555317eb037bcf791abd45a11d
libogarza <63701558+libogarza@users.noreply.github.com>
2020-04-19 18:51:38 -0500

    Update random2.cpp

--- random2.cpp
+++ random2.cpp
@@ -1,45 +1,50 @@
-//************************************************************
-// Team #10    CSCI/CMPE 1370    Spring 2020    Homework 3 
+//****************************************************************************** 
+// Team #  10        CSCI/CMPE 1370            Spring2020            Homework # 3 
 // Liborio Garza
-// Pablo Flores
-//************************************************************
+// Pablo J. Flores Jr. 
+//******************************************************************************
 #include <iostream>
 #include <cmath>
 #include <string>
 #include <stdlib.h>
-#include "functions.h"
+#include "HF.h"
 using namespace std;
 void titlemenu();
 int gcd(int,int);
 void add();
 void subtract();
-void GetRational(int *num,int *den);
-void reduce(int *num,int *den);
-void AddRational(int *num,int *den,int num1,int den1,int num2,int den2);
-void SubtractRational(int *num,int *den, int num1,int den1,int num2,int den2);
+void GetRational(int &,int &);
+void reduce(int,int);
+void AddRational(int &,int &,int,int,int,int);
+void SubtractRational(int &,int &,int,int,int,int);
 void DisplayRational(int,int);
 int main()
 {
-    char letter;
-    titlemenu();
-    cout<<endl;
-    cout<<"Enter a Option: ";
-    cin>>letter;
-    cout<<endl;
-    system("CLS");
-    if(letter=='A'||letter=='a')
+    char letter,ans;
+    while(true)
     {
-        add();
+        titlemenu();
+        cout<<endl;
+        cout<<"Enter a Option: ";
+        cin>>letter;
+        cout<<endl;
+        system("CLS");
+        if(letter=='A'||letter=='a')
+        {
+            add();
+        }
+        else if(letter=='S'||letter=='s')
+        {
+            subtract();
+        }
+        else
+        {
+            break;
+        }
     }
-    else if(letter=='S'||letter=='s')
-    {
-        subtract();
-    }
-    else
-    {
-        cout<<"Thank you for using the rational calculator!!!!"<<endl;
-    }
+    cout<<"Thank you for using our Rational Calculator!!!";
     cout<<endl;
     system("pause");
     return 0;
 }
+
